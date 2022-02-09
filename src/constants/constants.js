7c2cb6787515a7389696da49e0a37bbb02227cb5
manutdmohit <saudmohit@gmail.com>
2022-02-09 16:29:04 +0545

    changes in image

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -3,7 +3,7 @@ export const projects = [
     title: 'E-Commerce',
     description:
       "While building it you're going to learn many advanced React & JavaScript topics, as well as how to use Sandbox for card transactions. On top of that, at the end of the video, you will have this unique and complex webshop app that you will be able to add to your portfolio. And trust me, e-commerce applications are impressive.",
-    image: '/images/ecommerce.png',
+    image: '/images/1.png',
     tags: ['React', 'Redux', 'JavaScript', 'Express', 'Mongo'],
     source: ' https://nepalshop.herokuapp.com/',
     visit: 'https://github.com/manutdmohit/ecommercewebsite',
@@ -13,7 +13,7 @@ export const projects = [
     title: 'Music Events',
     description:
       ' Create your own events or view other user events. This site uses Strapi as a backend. Next.js is built on top of Node.js enabling server-side rendering and generating static websites whereas Strapi is an open-source headless CMS used for building fast and easily manageable APIs written in JavaScript.',
-    image: '/images/musicevents.png',
+    image: '/images/2.png',
     tags: ['JavaScript', 'Next', 'Strapi'],
     source: 'https://musicevents.vercel.app/',
     visit: 'https://github.com/manutdmohit/music-events-frontend-main',
@@ -24,7 +24,7 @@ export const projects = [
     title: 'BootCamp Backend',
     description:
       'Creating the backend for a bootcamp directory website. Consists of all the rest api functionalities.',
-    image: '/images/devcamper.png',
+    image: '/images/3.png',
     tags: ['JavaScript', 'Express', 'Mongo', 'NPM', 'Postman'],
     source: 'https://documenter.getpostman.com/view/13237400/UVeJM5g1',
     visit: 'https://github.com/manutdmohit/BootCamp',
@@ -33,7 +33,7 @@ export const projects = [
   {
     title: 'Weather App',
     description: 'This is an Express App for checking weather using API.',
-    image: '/images/weather.png',
+    image: '/images/4.png',
     tags: ['JavaScript', 'Express', 'API', 'Heroku'],
     source: 'http://weather-app-bymohit.herokuapp.com/',
     visit: 'https://github.com/manutdmohit/herokuweatherapp',
