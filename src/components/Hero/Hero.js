ac30fc23ad0398225a999adfef97ddcaf094415b
manutdmohit <saudmohit@gmail.com>
2022-02-09 11:38:12 +0545

    Hero-section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,32 @@
 import React from 'react';
+import Link from 'next/link';
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from '../../styles/GlobalComponents';
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <Section>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome To <br />
+        My Personal Portfolio
+      </SectionTitle>
+
+      <SectionText>
+        Believe In: "The Success That Comes After The Hardwork Gives The Best
+        Taste."
+      </SectionText>
+    </LeftSection>
+
+    <Button onClick={() => (window.location = 'mailto:saudmohit@gmail.com')}>
+      Learn More
+    </Button>
+  </Section>
 );
 
 export default Hero;
